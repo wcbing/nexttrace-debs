29cf55a6d53818f1e1461a845e01b9c22a1925b7
wcbing <cbing2002@outlook.com>
2025-01-16 23:54:14 +0800

    更改为外部触发器触发

--- build.sh
+++ build.sh
@@ -1,9 +1,7 @@
 #!/bin/sh
 
-PACKAGE="nexttrace"
-REPO="nxtrace/NTrace-core"
-
-VERSION="$(cat tag)"
+TAG="$1"
+VERSION=$(echo $TAG | sed 's/^v//')
 
 ARCH="amd64 arm64"
 AMD64_FILENAME="nexttrace_linux_amd64"
@@ -11,20 +9,20 @@ ARM64_FILENAME="nexttrace_linux_arm64"
 
 get_url_by_arch() {
     case $1 in
-    "amd64") echo "https://github.com/$REPO/releases/latest/download/$AMD64_FILENAME" ;;
-    "arm64") echo "https://github.com/$REPO/releases/latest/download/$ARM64_FILENAME" ;;
+    "amd64") echo "https://github.com/nxtrace/NTrace-core/releases/download/$TAG/$AMD64_FILENAME" ;;
+    "arm64") echo "https://github.com/nxtrace/NTrace-core/releases/download/$TAG/$ARM64_FILENAME" ;;
     esac
 }
 
 build() {
     # Prepare
-    BASE_DIR="$PACKAGE"_"$VERSION"-1_"$1"
+    BASE_DIR="nexttrace"_"$VERSION"-1_"$1"
     cp -r templates "$BASE_DIR"
     sed -i "s/Architecture: arch/Architecture: $1/" "$BASE_DIR/DEBIAN/control"
     sed -i "s/Version: version/Version: $VERSION-1/" "$BASE_DIR/DEBIAN/control"
     # Download and move file
-    curl -sLo "$BASE_DIR/usr/bin/$PACKAGE" "$(get_url_by_arch $1)"
-    chmod 755 "$BASE_DIR/usr/bin/$PACKAGE"
+    curl -sLo "$BASE_DIR/usr/bin/nexttrace" "$(get_url_by_arch $1)"
+    chmod 755 "$BASE_DIR/usr/bin/nexttrace"
     # Build
     dpkg-deb --build --root-owner-group "$BASE_DIR"
 }
